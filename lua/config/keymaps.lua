b4be1dec7a45846398c4d3471d040eab57b9d8e4
9elt <nelt.primary@gmail.com>
2024-04-26 13:06:21 +0200

    open terminal map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 local map = vim.keymap.set
 
 map("n", "<leader>e", vim.diagnostic.open_float)
+
+local Util = require("lazyvim.util")
+
+local lazyterm = function()
+  Util.terminal(nil, { cwd = Util.root() })
+end
+
+map("n", "<c-,>", lazyterm, { desc = "Terminal (root dir)" })
+map("t", "<C-,/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
